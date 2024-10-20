ff8e9d057e75db2800ceb97c6bd375abab561fac
Kaito0523 <kaito.tanabe0523@gmail.com>
2024-10-21 00:47:11 +0900

    リクエストファイルの作成

---
+++ app/Http/Requests/MemoRequest/UpdateShoppingMemoRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\MemoRequest;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateShoppingMemoRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'title' => 'required|string',
+            'content' => 'required|string',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'タイトルは必須です',
+            'content.required' => '何か記入してください',
+        ];
+    }
+}
